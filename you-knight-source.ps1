f81c083fb39963835dd4a2b347c52e0534fbece5
Christian Belarde <cjbelarde253@gmail.com>
2023-11-19 23:34:46 -0800

    Set up the sleep timer for pacing between messages

--- you-knight-source.ps1
+++ you-knight-source.ps1
@@ -12,3 +12,22 @@ Version 1.0
 Production Began Nov, 2023 
 #>
 
+# Setting up the sleep timer function for message pacing
+function sleepTimer {
+    param($seconds)
+    Start-Sleep -Seconds ($seconds)
+}
+
+# This is supposed to hide the file path and make things look a little prettier in the console, needs work
+function prompt { 
+    $p = Split-Path -leaf -path (Get-Location)  
+    \"$p> \" 
+}
+
+
+
+Write-Host "This is a test message"
+sleepTimer 4
+Write-Host "This is a test message"
+sleepTimer 2
+Clear-Host
