dd4de88045d0d69320d8302062908ae41b5143e5
Christian Belarde <cjbelarde253@gmail.com>
2023-11-19 22:49:56 -0800

    Initial commit

---
+++ you-knight-source.ps1
@@ -0,0 +1,14 @@
+<#
+.SYNOPSIS
+A simple, text-based adventure game written in PowerShell
+ 
+.DESCRIPTION
+This script implements a short game using PowerShell. The game is a text-based adventure game set in the medival era. 
+You play as an aspiring young knight, sent out by your king on an adventure to find Blargh, hero of the kingdom, who has mysteriously gone missing.
+
+.NOTES
+Author: Christian Belarde 
+Version 1.0
+Production Began Nov, 2023 
+#>
+
