cc79bdb002e0a0921880b9bae970cf55e764a17b
Christian Belarde <cjbelarde253@gmail.com>
2023-11-20 17:15:56 -0800

    Starting additions to the begin journey function

--- you-knight-source.ps1
+++ you-knight-source.ps1
@@ -46,8 +46,9 @@ function writeFastScroll {
 }
 
 
+
 function intro {
-    write "   
+   <# write "   
  _______      _                                                   
 (_______)    | |                                                  
  _         __| |  ___    ____     ____  _____  ____   _____   ___ 
@@ -77,15 +78,52 @@ function intro {
     ----      ----------  ------------      ----   ----  ----   ----- --------  ------------ ----    ----     ----     
     ****       ********   ************      ****    **** ****    **** ********  ************ ****    ****     ****     
     "
+       for($i = 1; $i -le 8; $i++ ) {
+        sleepTimerM 500
+        write ""
+    }
+
+
+    writeFastScroll "The ruler of these lands, the mighty king, declares to you in calm, powerful voice, your orders....`n`n"
+    sleepTimerS 1
+    writeFastScroll '"This Kingdom used to peaceful. Now,'
+    sleepTimerS 1
+    writeFastScroll  ' its a wreck."'
+    sleepTimerS 1
+    writeFastScroll `n'"Our forces are already spread too thin, this cannot last"'
+    sleepTimerS 1
+    writeFastScroll `n'"Our great defender, Blargh, the most powerful knight this land has ever known, has gone missing"'
+    sleepTimerS 2
+    writeFastScroll `n'"Find him, o brave knight. You must find Blargh, and see that he returns to the kingdom"'
+    sleepTimerS 2
+    writeFastScroll `n'"And you must do it quickly...."'
+    sleepTimerS 2
+    writeFastScroll `n'"Or else I fear...."'
+    sleepTimerS 2
+    writeFastScroll `n'"There may not be a kingdom much longer"'
+#>
+    Read-Host "Begin Your Journey? (Y/N)"
+        if (Read-Host -eq "Y") {
+
+        } 
 }
 
-writeFastScroll "Perhaps this was the only way"
+function journeyStart {
+    write "The journey has begun"
+}
 
 
 
 
 
 
+
+
+
+
+
+
+intro
 
 
 
